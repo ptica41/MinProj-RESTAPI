3e975e5eee839938552c6f63001923f340dd28f0
ptica41 <iisolovyev@gmail.com>
2023-08-24 16:51:39 +0300

    rebuild git

--- minproj/Recipients/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecipientsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Recipients'
-
-    def ready(self):
-        import Recipients.signals
